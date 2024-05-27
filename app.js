b2613bc85bf44dfac14d33a2ae7d09126cbedffb
thatzerroguy <ndukaugochukwu9@gmail.com>
2024-05-27 14:36:12 +0100

    main

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' this is cloud computing DEVOPS ; CI-CD PIPELINE STEP 1 !'));
+app.get('/', (req, res) => res.send(
+    ' this is cloud computing DEVOPS ; CI-CD PIPELINE created with AWS, This page indicates success.',
+    'Name: Nduka Ugochukwu Chinedu',
+    'Matric NO.: VUG/CSC/21/5784'
+));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
