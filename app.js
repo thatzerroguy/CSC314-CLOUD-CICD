3fb99ce679eaf85dc7bcd00adf4d5d1bbe5e1125
thatzerroguy <ndukaugochukwu9@gmail.com>
2024-06-13 16:25:39 +0100

    main

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(
-    ' this is cloud computing DEVOPS ; CI-CD PIPELINE created with AWS, This page indicates success. Name: NDUKA UGOCHUKWU CHINEDU Matric NO.: VUG/CSC/21/5784 This is to test for manual approval',));
+// app.get('/', (req, res) => res.send(
+//     ' this is cloud computing DEVOPS ; CI-CD PIPELINE created with AWS, This page indicates success. Name: NDUKA UGOCHUKWU CHINEDU Matric NO.: VUG/CSC/21/5784 This is to test for manual approval',));
+
+app.use(express.static(path.join(__dirname, 'payment')));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
